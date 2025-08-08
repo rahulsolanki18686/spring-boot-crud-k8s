0eb51e437603e4e28bc8afac171a55e3aae4fb09
rahulsolanki18686 <rahulsolanki.softwaredeveloper@gmail.com>
2025-08-08 20:56:00 +0530

    first commit

---
+++ src/main/java/com/javatechie/entity/Product.java
@@ -0,0 +1,23 @@
+package com.javatechie.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Entity
+@Table(name = "PRODUCT_TBL")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String code;
+    private String name;
+    private int quantity;
+    private String availableDC;
+}
